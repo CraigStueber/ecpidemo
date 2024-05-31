40f8bbf958efd668c62304ee1421caf2a5412467
CraigStueber <craigstueber@gmail.com>
2024-05-30 20:24:38 -0400

    ready to go

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import Logo from "./assets/ECPISeal.png";
+
 function App() {
   return (
     <div>
       <h1>ECPI React Demo</h1>
+
       {/* Add link takes you to https://www.ecpi.edu/  */}
+
       <img src={Logo} alt="ECPI Seal" />
+
       {/* Add link takes you to https://www.ecpi.edu/programs/software-development-bachelor-degree */}
+
       {/* Add link takes you to https://www.ecpi.edu/how-to-apply*/}
     </div>
   );
